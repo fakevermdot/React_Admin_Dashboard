1b8a4b71e20bbbda4547214d7c2d659a1d0b90a7
fakeveldmort <dxgamergr@gmail.com>
2023-05-10 11:41:48 +0000

    third commit

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -4,26 +4,39 @@ import { userColumn, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import {db} from "../../firebase"
-import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { getDocs, collection, deleteDoc, doc, onSnapshot} from "firebase/firestore";
 const DataTable = () => {
   const [data, setData] =useState([]);
 
   useEffect(() => {
-    const fetchData = async () =>{
-      let list = []
-      try{
-        const querySnapshot = await getDocs(collection (db, "users"));
-        querySnapshot.forEach((doc) => {
-          list.push({id: doc.id, ...doc.data()})
-        });
-        setData(list)
-      }catch(err){
-        console.log(err)
-      }
-    };
-    fetchData()
+    // const fetchData = async () =>{
+    //   let list = []
+    //   try{
+    //     const querySnapshot = await getDocs(collection (db, "users"));
+    //     querySnapshot.forEach((doc) => {
+    //       list.push({id: doc.id, ...doc.data()})
+    //     });
+    //     setData(list)
+    //   }catch(err){
+    //     console.log(err)
+    //   }
+    // };
+    // fetchData()
+
+    //listener for real time snapshot
+    const unsub = onSnapshot(collection(db, "users"), (snapShot) => {
+      let list = [];
+      snapShot.docs.forEach(doc => {
+        list.push({id:doc.id, ...doc.data()})
+      });
+      setData(list)
+    }, (error) => {
+      console.log(error)
+    });
+    return () => {
+      unsub()
+    }
   },[])
-  console.log(data)
   const handleDelete = async(id) => {
     try{
       await deleteDoc(doc(db, "users", id))
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -4,17 +4,21 @@ import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { KeyboardArrowDown } from "@mui/icons-material";
 const Widget = ({ type }) => {
   let data;
-  // temporary amount
-  const amount = 100;
-  const diff = 100;
+  const [amount, setAmount] = useState(null)
+  const [diff, setDiff] = useState(null)
   switch (type) {
     case "user":
       data = {
         title: "USERS",
         isMoney: false,
         link: "See all users",
+        query: "users",
         icon: (
           <PersonOutlinedIcon
             className="icon"
@@ -28,9 +32,15 @@ const Widget = ({ type }) => {
         title: "ORDERS",
         isMoney: false,
         link: "View all orders",
-        icon: <ShoppingCartIcon className="icon" style={{
-            backgroundColor: "rgba(218, 165, 32, 0.2)",
-            color: "goldenrod",}}/>,
+        icon: (
+          <ShoppingCartIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
       };
       break;
     case "earning":
@@ -38,23 +48,58 @@ const Widget = ({ type }) => {
         title: "EARNING",
         isMoney: true,
         link: "View net earning",
-        icon: <MonetizationOnIcon className="icon"  style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}/>,
+        icon: (
+          <MonetizationOnIcon
+            className="icon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
       };
       break;
-    case "balance":
+    case "product":
       data = {
-        title: "BALANCE",
-        isMoney: true,
+        title: "product",
+        query: "products",
         link: "See Details",
-        icon: <AccountBalanceWalletIcon className="icon"   style={{
-            backgroundColor: "rgba(128, 0, 128, 0.2)",
-            color: "purple",
-          }}/>,
+        icon: (
+          <AccountBalanceWalletIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
       };
       break;
     default:
       break;
-  };
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const today = new Date();
+      const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+      const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+
+      const lastMonthQuery = query(
+        collection(db, data.query),
+        where("timeStamp", "<=", today),
+        where("timeStamp", ">", lastMonth)
+      );
+      const prevMonthQuery = query(
+        collection(db, data.query),
+        where("timeStamp", "<=", lastMonth),
+        where("timeStamp", ">", prevMonth)
+      );
+      const lastMonthData = await getDocs(lastMonthQuery);
+      const prevMonthData = await getDocs(prevMonthQuery);
+
+      setAmount(lastMonthData.docs.length)
+      setDiff(((lastMonthData.docs.length - prevMonthData.docs.length) / prevMonthData.docs.length) *100)
+
+    }
+    fetchData();
+  }, []);
   return (
     <div className="widget">
       <div className="left">
@@ -65,8 +110,8 @@ const Widget = ({ type }) => {
         <span className="link">{data.link}</span>
       </div>
       <div className="right">
-        <div className="percentage positive">
-          <KeyboardArrowUpIcon />
+        <div className={`percentage ${diff < 0 ? "'negative" : "positive"}`}>
+          {diff > 0 ? <KeyboardArrowUpIcon /> : <KeyboardArrowDown/>}
           {diff}%
         </div>
         {data.icon}
